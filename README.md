1cfae8e75df75e5a2293cba343e5a90af9d3032b
王宝同 <baotong.wang@qunar.com>
2016-06-16 16:59:37 +0800

    0.0.1 push

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # package-module-as-relative-plugin
-This plugin will resolve the relative path foramt list this require('local/file.js')
+此插件支持以下的require写法
+
+## Example
+在fekit项目中，经常会加载本地相对路径下的文件，写法为
+require('index')
+require('index.js')
+require('local/index')
+require('local/index.js')
+
+而这种写法在webpack以及node的常规require模式中是不支持的，如果遇到了开头是一个单词的require，那么会认为这里加载的是一个module，webpack会额外尝试使用resolve.extensions中配置的alias去替换再解析。所以以上的写法在所有支持的方法都找不到文件的时候就会throw error。
+
+这个插件就是为了支持这种不规范的写法，在不改动现有代码的情况下兼容fekit项目代码。
+
+Ok，就是这样
---
+++ index.js
@@ -0,0 +1,43 @@
+/**
+ *	@author: baotong.wang@hotmail.com
+ *	@lastModiftDate: 2016-06-16
+ *	@fileoverview: add a relative reference resolver to the webpack compile process,
+ *	@dependence：webpack
+ *	@other：bind the resolve to the webpack 'after-resolvers' process
+ */
+
+function relativeResolver() {}
+
+relativeResolver.prototype.apply = function(resolver) {
+	resolver.plugin('module', function(request, callback) {
+		var firstChar = request.request[0],
+			isIgnore = firstChar == '/' || firstChar == '\\' || firstChar == '.';		
+		
+		// only check the require that start with character.
+		// if it already a relative path then skip it
+		if (isIgnore || request.directory) return callback();
+
+		// change to local relative format and check again
+		var path = './' + request.request;
+
+		return this.doResolve(['file'], {
+			path: request.path,
+			request: path,
+			query: request.query
+		}, callback, true);
+	});
+}
+
+function ModuleAsRelativePlugin() {}
+
+// for the detail handle process see WebpackOptionsApply.js file under webpack/lib
+ModuleAsRelativePlugin.prototype.apply = function(compiler) {
+	compiler.plugin('after-resolvers', function(compiler) {
+		// add customer resolvers
+		compiler.resolvers.normal.apply(new relativeResolver())
+		compiler.resolvers.context.apply(new relativeResolver())
+		compiler.resolvers.loader.apply(new relativeResolver())
+	});
+};
+
+module.exports = ModuleAsRelativePlugin;
